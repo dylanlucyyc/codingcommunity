8df43dc3943c67427250a121ce338ded2b9fead7
Dylan Luc <122325393+nguydluc@users.noreply.github.com>
2025-03-09 19:07:18 -0600

    Fix the logo in all layouts and pages.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,18 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Box } from "@mui/material";
+import logoImg from "../logo.png";
 
-function Logo(disableLink = false, sx) {
+function Logo({ disabledLink = false, sx }) {
   const logo = (
-    <Box sx={{ width: 200, height: 40, ...sx }}>Coding Community</Box>
+    <Box sx={{ width: 40, height: 40, ...sx }}>
+      <img src={logoImg} alt="logo" width="100%" />
+    </Box>
   );
 
-  if (disableLink) {
+  if (disabledLink) {
     return <>{logo}</>;
   }
+
   return <RouterLink to="/">{logo}</RouterLink>;
 }
 
