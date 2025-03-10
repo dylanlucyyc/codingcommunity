cdd830a02da76e9813edb53b8a3d9d6125eedb79
Dylan Luc <122325393+nguydluc@users.noreply.github.com>
2025-03-09 18:52:31 -0600

    Update default MUI theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
+import ThemeProvider from "./theme";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Router />
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   );
